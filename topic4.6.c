2a271f24d42fceab40ed7308de1fd7e670e48f74
meettrapasiya <trapasiyameet78@gmail.com>
2024-06-26 12:58:16 +0530

    Add files via upload

---
+++ topic4.6.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main()
+{
+	int x,y,xy;
+	printf("enter value x=");
+	scanf("%d",&x);
+	printf("enter value y=");
+	scanf("%d",&y);
+	if(x>y)
+	
+	{
+		printf("x is maximum");
+		
+	}
+	
+	printf("y is ,maximum");
+}
+
